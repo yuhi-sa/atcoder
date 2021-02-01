8731049be498f209b0d874db025746d23fbf888c
yuhi-sa <62089243+yuhi-sa@users.noreply.github.com>
2021-02-01 16:29:35 +0900

    190fix

--- ABC190/d.py
+++ ABC190/d.py
@@ -1,29 +1,21 @@
-import math
 # N = int(input())
-N = 12
+n = 1
 
-# 差1の等差数列の和aは初項,nは項数
-def sumsum(a,n):
-    return int(n*(2*a+n-1)/2)
+def check(n,ans,l):
+    tmp = abs(n*n/l-l+1)
+    if tmp%2 == 0:
+        ans+=1
+    return ans
 
-# 解の個数
-def ansans(a,N):
-    tmp = 0
-    val1 = -(2*a-1)+ math.sqrt((2*a-1)*(2*a-1)+8*N)
-    val2 = -(2*a-1)+ math.sqrt((2*a-1)*(2*a-1)+8*N)
-    if val1.is_integer() and val1 >=0:
-        tmp+=1
-    if val2.is_integer() and val1 >=0:
-        tmp+=1
-    return tmp
-
-
-
-# 答え
 ans = 0
+x = 0
+while(x*x<=n*n):
+    x += 1
+    if n*n%x == 0:
+        y = int(n*n/x)
+        ans = check(n,ans,x)
+        if y!= x:
+            ans = check(n,ans,y)
+    print(x,y,ans)
 
-for a in range(N+1):
-    ans += sumsum(a,N)
-
-print(ans*2)
-
+print(ans)
